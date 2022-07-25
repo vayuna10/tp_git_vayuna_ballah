c50fade8fa22adbfc0ac6a71693f3b80fa6abdeb
vayuna10 <109500416+vayuna10@users.noreply.github.com>
2022-07-25 23:02:21 +0400

    Add files via upload

---
+++ code.py
@@ -0,0 +1,22 @@
+import sys
+
+def add(a,b):
+        sum = a + b
+        print(sum)
+
+a = int( sys.argv[1] )
+b = int( sys.argv[2] )
+
+
+try:
+        a = int( sys.argv[1] )
+        b = int( sys.argv[2] )
+except IndexError:
+        print("ERROR! 2 Numerical value")
+        a = int(input("Please insert the first value: "))
+        b = int(input("Please insert the second value: "))
+        add(a,b)
+        raise(SystemExit)
+
+
+add(a,b)

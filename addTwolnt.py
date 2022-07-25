1447cf0628bfb98da8eedd761eb9240843489c5d
vayuna10 <ballahvayuna@gmail.com>
2022-07-25 16:36:20 +0400

    This code has been updated.

--- addTwolnt.py
+++ addTwolnt.py
@@ -3,15 +3,22 @@
 import sys
 
 def add(a,b):
-sum = a + b
-print(sum)
+	sum = a + b
+	print(sum)
+
+a = int( sys.argv[1] )
+b = int( sys.argv[2] )
+
 
 try:
-a =int( sys.argv[1] )
-b =int( sys.argv[2] )
+	a = int( sys.argv[1] )
+	b = int( sys.argv[2] )
 except IndexError:
-print("Add only 2 Arguments")
-raise(SystemExit)
+	print("ERROR! 2 Numerical value")
+	a = int(input("Please insert the first value: "))
+	b = int(input("Please insert the second value: "))
+	add(a,b)
+	raise(SystemExit)
 
 
 add(a,b)
